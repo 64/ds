a7ac04ee039d9113f5982e1bd90f2084be2c7e2b
Matthew Taylor <mstaveleytaylor@gmail.com>
2017-08-28 03:05:23 +0100

    Add dlist functions

--- include/linked.h
+++ include/linked.h
@@ -6,6 +6,10 @@ struct slist_entry {
 	struct slist_entry *next;
 };
 
+struct dlist_entry {
+	struct dlist_entry *next, *prev;
+};
+
 // Compiler magic to find the original struct (taken from linux kernel)
 #define container_of(ptr, type, member) ({                      \
         const typeof(((type *)0)->member) *__mptr = (ptr);    \
@@ -14,9 +18,20 @@ struct slist_entry {
 #define slist_entry(ptr, type, member) \
 	container_of(ptr, type, member)
 
+#define dlist_entry(ptr, type, member) \
+	container_of(ptr, type, member)
+
 #define slist_get_next(ptr, member) ({ \
 	const struct slist_entry *__next = (ptr)->member.next; \
 	__next == NULL ? NULL : slist_entry(__next, typeof(*(ptr)), member); })
+
+#define dlist_get_next(ptr, member) ({ \
+	const struct dlist_entry *__next = (ptr)->member.next; \
+	__next == NULL ? NULL : dlist_entry(__next, typeof(*(ptr)), member); })
+
+#define dlist_get_prev(ptr, member) ({ \
+	const struct dlist_entry *__prev = (ptr)->member.prev; \
+	__prev == NULL ? NULL : dlist_entry(__prev, typeof(*(ptr)), member); })
 
 #define slist_set_next(ptr, member, next_entry) ({ \
 	const typeof(next_entry) __next_entry = (next_entry); \
@@ -24,5 +39,24 @@ struct slist_entry {
 	struct slist_entry *__next = (__next_entry == NULL) ? NULL : &__next_entry->member; \
 	__head->next = __next; })
 
-#define slist_foreach(head, member) \
-	for (; (head) != NULL; (head) = slist_get_next((head), member))
+#define dlist_set_next(ptr, member, next_entry)  ({ \
+	const typeof(next_entry) __next_entry = (next_entry); \
+	struct dlist_entry *__head = &(ptr)->member; \
+	struct dlist_entry *__next = (__next_entry == NULL) ? NULL : &__next_entry->member; \
+	__head->next = __next; \
+	if (__next != NULL) __next->prev = __head; })
+
+#define dlist_set_prev(ptr, member, prev_entry) ({ \
+	const typeof(prev_entry) __prev_entry = (prev_entry); \
+	struct dlist_entry *__head = &(ptr)->member; \
+	struct dlist_entry *__prev = (__prev_entry == NULL) ? NULL : &__prev_entry->member; \
+	__head->prev = __prev; \
+	if (__prev != NULL) __prev->next = __head; })
+
+#define slist_foreach(head, member, start) \
+	for (typeof(start) (head) = start; (head) != NULL; \
+		(head) = slist_get_next((head), member))
+
+#define dlist_foreach(head, member, start) \
+	for (typeof(start) (head) = start; (head) != NULL; \
+		(head) = dlist_get_next((head), member))
--- tests/test_linked.c
+++ tests/test_linked.c
@@ -3,53 +3,105 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-struct test_struct {
+struct test_slist {
 	int data;
-	struct slist_entry next;
+	struct slist_entry list;
+};
+
+struct test_dlist {
+	int data;
+	struct dlist_entry list;
 };
 
 MU_TEST(entry) {
-	struct test_struct t;
-	struct test_struct *p = slist_entry(&t.next, struct test_struct, next);
-	mu_assert_int_eq((uintptr_t)&t, (uintptr_t)p);
+	struct test_slist t1_s;
+	struct test_slist *p_s = slist_entry(&t1_s.list, struct test_slist, list);
+	mu_assert_int_eq((uintptr_t)&t1_s, (uintptr_t)p_s);
+
+	struct test_dlist t1_d;
+	struct test_dlist *p_d = dlist_entry(&t1_d.list, struct test_dlist, list);
+	mu_assert_int_eq((uintptr_t)&t1_d, (uintptr_t)p_d);
 }
 
 MU_TEST(get_next) {
-	struct test_struct t = { 0, { NULL } };
-	struct test_struct t2 = { 0, { NULL } };
-	mu_assert_int_eq(0, (uintptr_t)slist_get_next(&t, next));
-	t.next.next = &t2.next;
-	mu_assert_int_eq((uintptr_t)&t2, (uintptr_t)slist_get_next(&t, next));
+	struct test_slist t1_s = { 0, { NULL } };
+	struct test_slist t2_s = { 0, { NULL } };
+	mu_assert_int_eq(0, (uintptr_t)slist_get_next(&t1_s, list));
+	t1_s.list.next = &t2_s.list;
+	mu_assert_int_eq((uintptr_t)&t2_s, (uintptr_t)slist_get_next(&t1_s, list));
+
+	struct test_dlist t1_d = { 0, { NULL, NULL } };
+	struct test_dlist t2_d = { 0, { NULL, NULL } };
+	mu_assert_int_eq(0, (uintptr_t)dlist_get_next(&t1_d, list));
+	t1_d.list.next = &t2_d.list;
+	mu_assert_int_eq((uintptr_t)&t2_d, (uintptr_t)dlist_get_next(&t1_d, list));
+}
+
+MU_TEST(get_prev) {
+	struct test_dlist t1_d = { 0, { NULL, NULL } };
+	struct test_dlist t2_d = { 0, { NULL, NULL } };
+	mu_assert_int_eq(0, (uintptr_t)dlist_get_prev(&t2_d, list));
+	t2_d.list.prev = &t1_d.list;
+	mu_assert_int_eq((uintptr_t)&t1_d, (uintptr_t)dlist_get_prev(&t2_d, list));
 }
 
 MU_TEST(set_next) {
-	struct test_struct t = { 0, { NULL } };
-	struct test_struct t2 = { 0, { NULL } };
-	slist_set_next(&t, next, &t2);
-	mu_assert_int_eq((uintptr_t)&t2, (uintptr_t)slist_get_next(&t, next));
-	slist_set_next(&t, next, (struct test_struct *)NULL);
-	mu_assert_int_eq(0, (uintptr_t)slist_get_next(&t, next));
+	struct test_slist t1_s = { 0, { NULL } };
+	struct test_slist t2_s = { 0, { NULL } };
+	slist_set_next(&t1_s, list, &t2_s);
+	mu_assert_int_eq((uintptr_t)&t2_s, (uintptr_t)slist_get_next(&t1_s, list));
+	slist_set_next(&t1_s, list, (struct test_slist *)NULL);
+	mu_assert_int_eq(0, (uintptr_t)slist_get_next(&t1_s, list));
+
+	struct test_dlist t1_d = { 0, { NULL, NULL } };
+	struct test_dlist t2_d = { 0, { NULL, NULL } };
+	dlist_set_next(&t1_d, list, &t2_d);
+	mu_assert_int_eq((uintptr_t)&t2_d, (uintptr_t)dlist_get_next(&t1_d, list));
+	dlist_set_next(&t1_d, list, (struct test_dlist *)NULL);
+	mu_assert_int_eq(0, (uintptr_t)dlist_get_next(&t1_d, list));
+}
+
+MU_TEST(set_prev) {
+	struct test_dlist t1_d = { 0, { NULL, NULL } };
+	struct test_dlist t2_d = { 0, { NULL, NULL } };
+	dlist_set_prev(&t2_d, list, &t1_d);
+	mu_assert_int_eq((uintptr_t)&t1_d, (uintptr_t)dlist_get_prev(&t2_d, list));
+	dlist_set_prev(&t2_d, list, (struct test_dlist *)NULL);
+	mu_assert_int_eq(0, (uintptr_t)dlist_get_prev(&t2_d, list));
 }
 
 MU_TEST(foreach) {
-	struct test_struct t1 = { 0, { NULL } };
-	struct test_struct t2 = { 1, { NULL } };
-	struct test_struct t3 = { 2, { NULL } };
-	struct test_struct t4 = { 3, { NULL } };
-	slist_set_next(&t1, next, &t2);
-	slist_set_next(&t2, next, &t3);
-	slist_set_next(&t3, next, &t4);
-	struct test_struct *current = &t1;
-	int iterations = 0;
-	slist_foreach(current, next) {
-		mu_assert_int_eq(iterations++, current->data);
+	struct test_slist t1_s = { 0, { NULL } };
+	struct test_slist t2_s = { 1, { NULL } };
+	struct test_slist t3_s = { 2, { NULL } };
+	struct test_slist t4_s = { 3, { NULL } };
+	slist_set_next(&t1_s, list, &t2_s);
+	slist_set_next(&t2_s, list, &t3_s);
+	slist_set_next(&t3_s, list, &t4_s);
+	int iterations_s = 0;
+	slist_foreach(current_s, list, &t1_s) {
+		mu_assert_int_eq(iterations_s++, current_s->data);
+	}
+
+	struct test_dlist t1_d = { 0, { NULL, NULL } };
+	struct test_dlist t2_d = { 1, { NULL, NULL } };
+	struct test_dlist t3_d = { 2, { NULL, NULL } };
+	struct test_dlist t4_d = { 3, { NULL, NULL } };
+	dlist_set_next(&t1_d, list, &t2_d);
+	dlist_set_next(&t2_d, list, &t3_d);
+	dlist_set_next(&t3_d, list, &t4_d);
+	int iterations_d = 0;
+	dlist_foreach(current_d, list, &t1_d) {
+		mu_assert_int_eq(iterations_d++, current_d->data);
 	}
 }
 
 MU_TEST_SUITE(linked) {
 	MU_RUN_TEST(entry);
 	MU_RUN_TEST(get_next);
+	MU_RUN_TEST(get_prev);
 	MU_RUN_TEST(set_next);
+	MU_RUN_TEST(set_prev);
 	MU_RUN_TEST(foreach);
 }
 
